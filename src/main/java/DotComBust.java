3e5dd2e40039eb1f3d39789b5ad421d09055f132
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2022-11-18 19:53:30 +0200

    added helper class

--- src/main/java/DotComBust.java
+++ src/main/java/DotComBust.java
@@ -1,5 +1,5 @@
 import java.util.*;
-/* Create 3 DotComs, give each of them a name.
+     /*Create 3 DotComs, give each of them a name.
        Call a setter method on each DotCom instance.
        Put the DotComs on a grid, and do it for all 3 instances.
        Check each user guess with all instances.
@@ -11,6 +11,31 @@ public class DotComBust {
     private int numOfGuesses = 0;
 
     private void setupGame() {
+        DotCom one = new DotCom();     //make 3 dotcoms, give them names
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+
+        dotComsList.add(one);              //put them in an ArrayList
+        dotComsList.add(two);
+        dotComsList.add(three);
+
+        System.out.println("Your goal is to sink the 3 dotcoms.");     //brief instructions for user
+        System.out.println("Pets.com, eToys.com, Go2.com");
+        System.out.println("Try to sink them all in the fewest possible guesses.");
+        
+        for (DotCom dotComToSet : dotComsList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            
+        }
+
+
+
+
+
+
 
     }
 
